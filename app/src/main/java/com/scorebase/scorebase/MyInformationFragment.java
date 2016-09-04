ebfbb70094c416bd934a4361b3a84106078fca39
Lee youngtaek <ting4602@gmail.com>
2016-09-04 21:16:51 +0900

    Home

--- app/src/main/java/com/scorebase/scorebase/MyInformationFragment.java
+++ app/src/main/java/com/scorebase/scorebase/MyInformationFragment.java
@@ -10,6 +10,7 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -132,6 +133,7 @@ public class MyInformationFragment extends Fragment {
 
                     // Upload Profile Image
                     Uri uri = getImageUri(getContext(), bitmap);
+                    Log.d("WWW", "onActivityResult: " + uri);
                     UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder()
                             .setPhotoUri(uri)
                             .build();
