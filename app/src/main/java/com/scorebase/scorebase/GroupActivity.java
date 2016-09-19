a77cfc604063fb673c23c7476373f9ba8eef8803
Lee youngtaek <ting4602@gmail.com>
2016-09-20 07:28:44 +0900

    GroupActivity:FloatingActionButton

--- app/src/main/java/com/scorebase/scorebase/GroupActivity.java
+++ app/src/main/java/com/scorebase/scorebase/GroupActivity.java
@@ -3,22 +3,47 @@ package com.scorebase.scorebase;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.melnykov.fab.FloatingActionButton;
 
 public class GroupActivity extends AppCompatActivity {
+
+    // Views
+    private FloatingActionButton floatingActionButton;
+    private TextView groupName, accessScope;
+
+    // Data
+    private Group group;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
 
+        // Get Group Data
         Intent intent = getIntent();
-        Group group = (Group) intent.getSerializableExtra("group");
+        group = (Group) intent.getSerializableExtra("group");
 
-        TextView groupName = (TextView)findViewById(R.id.text_view_group_name);
-        TextView accessScope = (TextView)findViewById(R.id.text_view_access_scope);
+        // View Reference
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        groupName = (TextView)findViewById(R.id.text_view_group_name);
+        accessScope = (TextView)findViewById(R.id.text_view_access_scope);
 
+        // View Setting
+        floatingActionButton.setColorNormalResId(R.color.colorAccent);
+        floatingActionButton.setColorPressedResId(R.color.colorFloatingActionButton);
         groupName.setText(group.getName());
         accessScope.setText(group.getAccessScope());
+
+        // View Event
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
